cafcd16bcb70fb16e527a232021c6b00466062c8
laggerk24 <laggerk24@gmail.com>
2024-08-31 02:01:18 +0530

    Skills and Contact Page

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,8 @@
 import './App.css'
 import Header from './components/header/Header'
 import Home from './components/home/Home'
+import About from './components/about/About'
+import Skills from './components/skills/Skills'
 
 function App() {
 
@@ -8,7 +10,9 @@ function App() {
     <>
       <Header />
       <main className="main">
-        <Home/>
+        <Home />
+        <About />
+        <Skills />
       </main>
     </>
   )
